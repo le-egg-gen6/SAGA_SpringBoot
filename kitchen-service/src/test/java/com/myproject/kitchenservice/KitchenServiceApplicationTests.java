5a886abc06065ad214d9d95f9bac012984de96e0
nguyenle <lenn@falcongames.com>
2025-01-09 14:08:17 +0700

    init project for implement saga-choreography design pattern for MSA

---
+++ kitchen-service/src/test/java/com/myproject/kitchenservice/KitchenServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myproject.kitchenservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KitchenServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
